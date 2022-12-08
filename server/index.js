77ea0b3771cdaaf0e8b7e36537c99c53d25f0ad0
Jazzuro <hjpachecocampos@gmail.com>
2022-12-08 17:50:36 -0500

    Added the search player functionality, you can retrieve the top played champions of a player. You can now send player data to a mySQL database when searching for a player for faster loading times. Added many useStates to load data as it is being fetched from APIs. You can now retrieve new data and retrieve old data, combine it and send it to the database to keep up to date data. Loading times were improved by 1-2 seconds.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,91 @@
 const express = require("express");
 const cors = require("cors");
-
+const mysql = require("mysql");
 const app = express();
 
 app.use(express.urlencoded({ limit: "100mb", extended: "true" }));
 app.use(express.json({ limit: "100mb" }));
 app.use(cors());
 
+const con = mysql.createConnection({});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 app.post("/player", (req, res) => {
-  console.log(req.body);
+  let basicPlayerData = req.body;
+  con.query(
+    "SELECT playerData FROM player WHERE puuid = '" +
+      basicPlayerData.puuid +
+      "'",
+    function (err, result) {
+      if (result.length >= 1) {
+        con.query(
+          "SELECT playerData FROM player WHERE puuid = '" +
+            basicPlayerData.puuid +
+            "'",
+          function (err, result) {
+            if (err) throw err;
+            res.send(
+              JSON.parse(
+                Object.values(JSON.parse(JSON.stringify(result)))[0].playerData
+              )
+            );
+          }
+        );
+        console.log("The player already exists");
+      } else res.send([]);
+    }
+  );
+});
+
+app.post("/addPlayer", (req, res) => {
+  let fullPlayerData = req.body;
+
+  con.query(
+    "INSERT INTO player(player_id, puuid, playerData) VALUES (DEFAULT, '" +
+      fullPlayerData.puuid +
+      "', '" +
+      JSON.stringify(fullPlayerData) +
+      "')",
+    function (err, result) {
+      if (err) throw err;
+      console.log("data sent!" + result);
+    }
+  );
 });
 
 app.get("/player", (req, res) => {});
 
+app.post("/updatePlayer", (req, res) => {
+  let fullPlayerData = req.body;
+  con.query(
+    "UPDATE player SET playerData = '" +
+      JSON.stringify(fullPlayerData) +
+      "' WHERE puuid = '" +
+      fullPlayerData.puuid +
+      "'",
+    function (err, result) {
+      if (err) throw err;
+      console.log("data updated!" + result);
+    }
+  );
+});
+
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
+
+//  con.query(
+//    "UPDATE player SET playerData = '" +
+//      JSON.stringify(fullPlayerData) +
+//      "' WHERE puuid = '" +
+//      fullPlayerData.puuid +
+//      "'",
+//    function (err, result) {
+//      if (err) throw err;
+//      console.log("data sent!" + result);
+//    }
+//  );
